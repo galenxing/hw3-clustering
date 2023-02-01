d8e4a1d50b2e884f17b6176bb218431945683bab
Galen Xing <gx2113@columbia.edu>
2023-02-01 01:37:24 -0800

    pytest and toml

--- cluster/__init__.py
+++ cluster/__init__.py
@@ -1,3 +1,7 @@
+"""
+HW3 Kmeans Clustering
+"""
+
 from .kmeans import KMeans
 from .silhouette import Silhouette
 from .utils import (
@@ -5,3 +9,5 @@ from .utils import (
         plot_clusters,
         plot_multipanel)
 
+
+__version__ = "0.0.1"
--- test/test_kmeans.py
+++ test/test_kmeans.py
@@ -1,1 +1,52 @@
 # Write your k-means unit tests here
+# write tests for bfs
+import pytest
+import sklearn.metrics
+import numpy as np
+from numpy.testing import assert_almost_equal
+
+from cluster import (
+        KMeans, 
+        Silhouette, 
+        make_clusters)
+
+def test_kmeans():
+    """
+    Testing Kmeans class
+    """
+    clusters, labels = make_clusters(k=3, scale=1)
+    km = KMeans(k=3)
+    km.fit(clusters)
+    pred = km.predict(clusters)
+    
+    assert len(np.unique(pred)) == 3
+    
+    km = KMeans(k=2)
+    km.fit(clusters)
+    pred = km.predict(clusters)
+    
+    assert len(np.unique(pred)) == 2
+    
+    
+        
+    assert km.get_error() > 0 
+    assert km.get_error() == km.error
+    
+    assert km.get_centroids().shape == (2, clusters.shape[1])
+
+    with pytest.raises(Exception):
+        km = KMeans(k=0)
+
+def test_silhouette():
+    clusters, labels = make_clusters(k=3, scale=1)
+    km = KMeans(k=3)
+    km.fit(clusters)
+    pred = km.predict(clusters)
+    
+    scores = Silhouette().score(clusters, pred)
+    
+    sk_sil = sklearn.metrics.silhouette_score(clusters, pred)
+    
+    assert_almost_equal(np.mean(scores), np.mean(sk_sil), 2)
+    
+    
